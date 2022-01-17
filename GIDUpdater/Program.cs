48c27dcb111956a319cb7705ee77da34840ea22b
Maciej B <Maciox55@gmail.com>
2022-01-17 15:46:47 -0500

    Added backup of the current GETIID directory for safe keeping. Added local GETIID version checking.

--- GIDUpdater/Program.cs
+++ GIDUpdater/Program.cs
@@ -1,12 +1,38 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.IO.Compression;
+using System.Reflection;
 
 namespace GIDUpdater
 {
     class Program
     {
+        
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var path = Directory.GetCurrentDirectory();
+
+            var version = AssemblyName.GetAssemblyName(@"GETIID.exe").Version;
+            Console.WriteLine(String.Format("Current GETIID Version: {0}", version));
+            //string version = versionInfo.FileVersion;
+            IEnumerable<String> files = Directory.GetFiles(path);
+            backupZIP("update_backup.zip", files); // Zip the GETIID version before downloading the 
+            
+        }
+        static void backupZIP(string fileName, IEnumerable<string> files)
+        {
+
+            var zip = ZipFile.Open(fileName, ZipArchiveMode.Create);
+
+            foreach (var file in files)
+            {
+                zip.CreateEntryFromFile(file, Path.GetFileName(file), CompressionLevel.Optimal);
+            }
+
+            zip.Dispose();
+
         }
     }
 }

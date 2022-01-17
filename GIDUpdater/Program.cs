3def5fac14af2c66fc4a7f281324e7a3617a1eb2
Maciej B <Maciox55@gmail.com>
2022-01-17 14:39:49 -0500

    Add project files.

---
+++ GIDUpdater/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace GIDUpdater
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
